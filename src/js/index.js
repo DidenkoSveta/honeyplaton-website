f6c68b0a3776386b3cb5d84e2397318cbac149b1
DidenkoSveta <garpia.lana@gmail.com>
2023-12-26 08:42:20 +0400

    realised scroll to top

--- src/js/index.js
+++ src/js/index.js
@@ -4,11 +4,13 @@ import '../scss/style.scss'; // Путь к файлу стилей
 import burgerMenu from './modules/burger-menu';
 import startAnimation from './modules/animation';
 import modal from './modules/modal';
+import scroll from './modules/scroll-to-top';
 
 // Использование функций после полной загрузки DOM
 document.addEventListener('DOMContentLoaded', () => {
   burgerMenu();
   startAnimation();
   modal();
+  scroll();
   
 });
