2682c93d1f8599497d7f7fb4148db0fb063abc49
DidenkoSveta <garpia.lana@gmail.com>
2023-12-27 16:17:52 +0400

    animation for footer logo

--- src/js/modules/logo-animation.js
+++ src/js/modules/logo-animation.js
@@ -3,7 +3,8 @@ import { gsap } from "gsap";
 export default function logo_animation() {
    const letters = document.querySelectorAll('.logo__letter');
    const svgPath = document.querySelector('.header__logo-svg path'); // Предполагая, что анимируется path в SVG
-
+   const lettersFooter = document.querySelectorAll('.logo__letter-footer');
+   const svgPathFooter = document.querySelector('.footer__logo-svg path');
    // Анимация для букв
    letters.forEach((letter, index) => {
       gsap.to(letter, {
@@ -17,12 +18,35 @@ export default function logo_animation() {
       });
    });
 
+   lettersFooter.forEach((letter, index) => {
+      gsap.to(letter, {
+         duration: 1,
+         ease: "power1.inOut",
+         color: "#DB9E36",
+         repeat: -1,
+         repeatDelay: 1.6,
+         yoyo: true,
+         delay: index * 0.1,
+      });
+   });
+
    // Анимация для SVG (анимация атрибута 'fill')
    if (svgPath) {
       gsap.to(svgPath, {
          duration: 1.5,
          ease: "power1.inOut",
          attr: { fill: "#FFD34E" },
+         repeat: -1,
+         repeatDelay: 1,
+         yoyo: true
+      });
+   }
+
+   if (svgPathFooter) {
+      gsap.to(svgPathFooter, {
+         duration: 1.5,
+         ease: "power1.inOut",
+         attr: { fill: "#DB9E36" },
          repeat: -1,
          repeatDelay: 1,
          yoyo: true
